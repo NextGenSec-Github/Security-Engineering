841c99108a5effae11fef58bf1cbbcd73fc046de
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-02-05 15:55:13 +0500

    Update Shellshock.sh

--- Network Security/IDS-IPS/Snort/Snort Rules/Shellshock.sh
+++ Network Security/IDS-IPS/Snort/Snort Rules/Shellshock.sh
@@ -11,3 +11,10 @@ alert udp $EXTERNAL_NET any -> $HOME_NET 53 (msg:"Possible Shellshock Attempt in
 
 # Rule to detect Shellshock attempt in generic TCP traffic
 alert tcp $EXTERNAL_NET any -> $HOME_NET any (msg:"Possible Shellshock Attempt in Generic TCP Traffic"; flow:to_server,established; content:"() {"; payload; sid:200004;)
+
+_____________
+NOTE
+_____________
+# Customize the $EXTERNAL_NET, $HOME_NET, $HTTP_SERVERS, and $HTTP_PORTS variables according to your network configuration.
+# These rules are basic examples and may generate false positives based on legitimate traffic. Fine-tune the rules based on your network's normal behavior and specific application context.
+# Shellshock detection often involves more sophisticated techniques, and these rules may need adjustment based on specific application deployment scenarios.
