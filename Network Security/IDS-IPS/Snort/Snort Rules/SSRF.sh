9471e7e4927673c4cf472f3c06f3c8cda7a9e6d4
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-02-05 15:36:58 +0500

    Update SSRF.sh

--- Network Security/IDS-IPS/Snort/Snort Rules/SSRF.sh
+++ Network Security/IDS-IPS/Snort/Snort Rules/SSRF.sh
@@ -10,3 +10,9 @@ alert tcp $EXTERNAL_NET any -> $HTTP_SERVERS $HTTP_PORTS (msg:"Potential SSRF in
 alert tcp $EXTERNAL_NET any -> $HOME_NET any (msg:"Potential SSRF in Generic TCP Traffic"; flow:to_server,established; pcre:"/http:\/\/internal-server\/access/i"; payload; sid:300003;)
 
 
+_____________
+NOTE
+_____________
+# Customize the $EXTERNAL_NET, $HTTP_SERVERS, $HTTP_PORTS, and $HOME_NET variables according to your network configuration.
+# These rules are basic examples, and you may need to adjust them based on the specific web application and SSRF attack vectors you want to protect against.
+# Regularly update and refine your rules based on the evolving threat landscape and changes in your network environment.
