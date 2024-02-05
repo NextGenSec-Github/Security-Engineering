b9a7edf92a9c608aed64db44b690e80ba27e8500
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-02-05 15:52:43 +0500

    Update File Inclusion-Traversal.sh

--- Network Security/IDS-IPS/Snort/Snort Rules/File Inclusion-Traversal.sh
+++ Network Security/IDS-IPS/Snort/Snort Rules/File Inclusion-Traversal.sh
@@ -11,3 +11,10 @@ alert tcp $EXTERNAL_NET any -> $HTTP_SERVERS $HTTP_PORTS (msg:"Possible Encoded 
 
 # Rule to detect file traversal attempts using encoded '../' in generic TCP traffic
 alert tcp $EXTERNAL_NET any -> $HOME_NET any (msg:"Possible Encoded File Traversal Attempt in Generic TCP Traffic"; flow:to_server,established; pcre:"/%2E%2E%2F/"; payload; sid:100004;)
+
+_____________
+NOTE
+_____________
+# Customize the $EXTERNAL_NET, $HOME_NET, $HTTP_SERVERS, and $HTTP_PORTS variables according to your network configuration.
+# These rules are basic examples and may generate false positives based on legitimate traffic. Fine-tune the rules based on your network's normal behavior and specific application context.
+# File inclusion and traversal detection often involve more sophisticated techniques, and these rules may need adjustment based on specific web application deployment scenarios.
