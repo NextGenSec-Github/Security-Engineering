0f386f21986bdaf7a23c7f8662129c052165cf38
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-02-05 15:34:50 +0500

    Update XSS.sh

--- Network Security/IDS-IPS/Snort/Snort Rules/XSS.sh
+++ Network Security/IDS-IPS/Snort/Snort Rules/XSS.sh
@@ -15,3 +15,9 @@ alert tcp $EXTERNAL_NET any -> $HTTP_SERVERS $HTTP_PORTS (msg:"Potential DOM-bas
 # Rule to detect potential XSS attacks in generic TCP traffic
 alert tcp $EXTERNAL_NET any -> $HOME_NET any (msg:"Potential XSS in Generic TCP Traffic"; flow:to_server,established; pcre:"/<script[\s\S]*?>.*<\/script>/iU"; payload; sid:200005;)
 
+_____________
+NOTE
+_____________
+# Customize the $EXTERNAL_NET, $HTTP_SERVERS, $HTTP_PORTS, and $HOME_NET variables according to your network configuration.
+# These rules are basic examples, and you may need to adjust them based on the specific web application and XSS attack vectors you want to protect against.
+# Regularly update and refine your rules based on the evolving threat landscape and changes in your network environment.
