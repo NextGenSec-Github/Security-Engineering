2e109acfc5270f6253b3f9c9546d3d0828327c89
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-02-06 11:15:05 +0500

    Update Snort Command Cheatsheet.sh

--- Network Security/IDS-IPS/Snort/Snort Command Cheatsheet.sh
+++ Network Security/IDS-IPS/Snort/Snort Command Cheatsheet.sh
@@ -1,1 +1,21 @@
+# | SNORT COMMAND CHEATSHEET |
 
+#NOTE: These are default locations for snort logs, config files, and rules:
+# /var/log/snort — is the default location of the log file
+# /etc/snort/snort.conf — default location of the configuration file
+# /etc/snort/rules/local.rules — default location of the local rules
+
+sudo snort -A full -r my.pcap -c local.rules -l . #Investigating a pcap file
+
+sudo snort -r ,snort.log.file> #reading dumped log file
+
+
+
+
+sudo nano local.rules #editing the local rule with a text editor, nano
+
+cat alert # reading the contents of the alert file
+
+sudo rm alert #deleting the alert file
+
+sudo rm <snort.log.file> #deleting the log file
