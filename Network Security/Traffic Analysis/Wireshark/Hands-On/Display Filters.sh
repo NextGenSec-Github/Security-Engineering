2c5a09ba9769a664b7e61a65c77cbaf5381ee38b
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-02-25 14:24:18 +0500

    Update Display Filters.sh

--- Network Security/Traffic Analysis/Wireshark/Hands-On/Display Filters.sh
+++ Network Security/Traffic Analysis/Wireshark/Hands-On/Display Filters.sh
@@ -1,5 +1,6 @@
 # =========================
 # Wireshark Display Filters
 # =========================
-# NOTE: What are Display Filters? Display filters in Wireshark are used to selectively display packets from a packet capture based on specific criteria. They allow you to focus on the packets that are relevant to your analysis and hide the rest. Display filters can be based on various packet attributes such as protocol, source or destination IP address, port numbers, packet length, and more.
+# NOTE: What are Display Filters? Display filters in Wireshark are used to selectively display packets from a packet capture based on specific criteria. They allow you to focus on the packets that are relevant to your analysis and hide the rest. 
+# Display filters can be based on various packet attributes such as protocol, source or destination IP address, port numbers, packet length, and more.
 
